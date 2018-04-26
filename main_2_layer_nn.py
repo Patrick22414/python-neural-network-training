ac28132e32e6312b028feb64ee27e8863a9b0680
Patrick <32917395+Patrick22414@users.noreply.github.com>
2018-04-26 10:11:19 +0800

    FC layer - added ReLU - w.i.p.

--- main_2_layer_nn.py
+++ main_2_layer_nn.py
@@ -1,5 +1,5 @@
 import numpy as np
-from util import cifar_10, fc_layer
+from util import cifar_10, layer
 import time
 
 n_pixel = 3072
@@ -7,8 +7,8 @@ n_mid = 100
 n_class = 10
 step_size = 1e-4
 
-layer_1 = fc_layer.FCLayer(n_pixel, n_mid, step_size, "softmax")
-layer_2 = fc_layer.FCLayer(n_mid, n_class, step_size, "softmax")
+layer_1 = layer.FCLayer(n_pixel, n_mid, step_size, "Softmax")
+layer_2 = layer.FCLayer(n_mid, n_class, step_size, "Softmax loss")
 
 batch = [None] * 5
 batch[0] = cifar_10.unpickle("data_batch_1")
@@ -19,19 +19,19 @@ batch[4] = cifar_10.unpickle("data_batch_5")
 n_train = 10000
 
 start = time.time()
-for usage in range(5):
-    for j in range(5):
+for usage in range(1):
+    for j in range(1):
         data = batch[j][b'data'] / 256
         label = batch[j][b'labels']
         for k in range(n_train):
             res_from_l1 = layer_1.train(data[k])
             res_from_l2 = layer_2.train(res_from_l1)
-            grad_from_l2 = layer_2.backprop(label[k])
-            grad_from_l1 = layer_1.backprop(label[k], previous_grad=grad_from_l2)
+            grad_from_l2 = layer_2.backprop(label=label[k])
+            grad_from_l1 = layer_1.backprop(upstream_grad=grad_from_l2)
 print("--- Training time: {0:.4f}s".format((time.time() - start)))
 
 batch_test = cifar_10.unpickle("test_batch")
-data_test = np.append((batch_test[b'data'] / 256), np.ones([10000, 1]), axis=1)
+data_test = batch_test[b'data'] / 256
 label_test = batch_test[b'labels']
 n_bingo = 0
 n_test = 10000
---
+++ util/layer.py
@@ -0,0 +1,97 @@
+import numpy as np
+
+
+class FCLayer:
+    def __init__(self, n_inputs: int, n_outputs: int, step_size: float, activation: str):
+        self.i = n_inputs  # number of inputs
+        self.o = n_outputs  # number of outputs
+        self.act_fun = activation  # type of activation function
+        self.step = step_size  # step size
+
+        self.w = np.zeros([self.o, self.i])  # weights matrix
+        self.x = np.zeros(self.i)  # input data
+        self.s = np.zeros(self.o)  # s = w.dot(x)
+        self.y = np.zeros(self.o)  # output
+        self.dy = np.zeros(self.o)
+
+    def train(self, data: np.ndarray):
+        self.x = data
+
+        if self.act_fun == "Softmax" or self.act_fun == "Softmax loss":
+            return self.softmax()
+        elif self.act_fun == "ReLU":
+            return self.relu()
+        else:
+            raise NotImplementedError
+
+    def backprop(self, label=None, upstream_grad=None):
+        if upstream_grad is not None:
+            self.dy = upstream_grad
+
+        if self.act_fun == "Softmax":
+            assert (label is None) and (upstream_grad is not None)
+            return self.softmax_bp()
+        elif self.act_fun == "Softmax loss":
+            assert (label is not None) and (upstream_grad is None)
+            return self.softmax_loss_bp(label)
+        elif self.act_fun == "ReLU":
+            assert (label is None) and (upstream_grad is not None)
+            return self.relu_bp()
+
+    def predict(self, data):
+        tmp = np.exp(np.matmul(self.w, data))
+        res = tmp / np.sum(tmp)
+        return res
+
+    def softmax(self):
+        """softmax forward compute"""
+        self.s = np.matmul(self.w, self.x)
+        s_exp = np.exp(self.s)
+        self.y = s_exp / np.sum(s_exp)
+        return self.y
+
+    def softmax_loss(self, label):
+        return self.softmax()[label]
+
+    def softmax_bp(self):
+        """softmax back-propagation"""
+        dy_ds = np.zeros([self.o, self.o])
+        for k in range(self.o):
+            dy_ds[k] = - self.y[k] * self.y
+            dy_ds[k, k] += self.y[k]
+        ds = np.matmul(dy_ds, self.dy)
+
+        dw = np.outer(ds, self.x)
+        dx = np.matmul(self.w.T, self.s)
+
+        self.w -= self.step * dw
+        return dx
+
+    def softmax_loss_bp(self, label):
+        """softmax loss back-propagation"""
+        dy = -1 / self.y[label]
+
+        ds = - self.y[label] * self.y
+        ds[label] += self.y[label]
+        ds *= dy
+
+        dw = np.outer(ds, self.x)
+        dx = np.matmul(self.w.T, self.s)
+
+        self.w -= self.step * dw
+        return dx
+
+    def relu(self):
+        self.s = np.matmul(self.w, self.x)
+        self.y = self.s
+        self.y[self.y < 0] = 0
+        return self.y
+
+    def relu_bp(self):
+        ds = np.ones_like(self.s)
+        ds[self.s < 0] = 0
+        dw = np.outer(ds, self.x)
+        dx = np.matmul(self.w.T, self.s)
+
+        self.w -= self.step * dw
+        return dx
